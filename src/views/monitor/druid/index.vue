136f74d2f7fd18eff1ac3361c254bb6c998b825d
DaenMax <1330166565@qq.com>
2023-04-23 18:59:24 +0800

    新增监控页面 删除其他页面

--- src/views/monitor/druid/index.vue
+++ src/views/monitor/druid/index.vue
@@ -4,11 +4,11 @@
 <script>
 import iFrame from "@/components/iFrame/index";
 export default {
-  name: "Admin",
+  name: "Druid",
   components: { iFrame },
   data() {
     return {
-      url: process.env.VUE_APP_MONITRO_ADMIN
+      url: process.env.VUE_APP_BASE_API + "/druid/login.html"
     };
   },
 };
